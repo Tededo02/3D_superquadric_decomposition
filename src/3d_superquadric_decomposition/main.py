db49f32e2529dfa604349024b93dfe652870ccf9
Edoardo Tedesco <edotededo@outlook.com>
2026-02-27 12:53:51 +0100

    chamfer distance one side and symmetric

--- src/3d_superquadric_decomposition/main.py
+++ src/3d_superquadric_decomposition/main.py
@@ -5,12 +5,10 @@ import visualization as vis
 import numpy as np
 import superquadric_sampling as samp
 from gair_ransac.inner_ransac import InnerRansacResult, inner_ransac, fit_superquadric_ls
+from point_cloud_utils import k_nearest_neighbors, chamfer_distance
 
-def main(argv: list[str] | None = None) -> int:
-    if argv is None:
-        argv = sys.argv[1:]
-    
-    colors: np.ndarray = []
+def create_and_estimate_supq():
+    colors: list[str] = []
     # add a superquadric to the scene, sample points then try to fit a superquadric to the points, visualize the results
     # lightblue is the original superquadric, lightgreen is the estimated superquadric
     test = SuperQuadricParams(1.2,5.2,0.7,0.5,2,[2,2,1],[5,5,5])
@@ -21,26 +19,41 @@ def main(argv: list[str] | None = None) -> int:
     colors.append("lightblue")
     #list_mesh.append(mesh2)
     #sampled_points = samp.sampling_sq(list_mesh, n_points=1000)
-    sampled_points_noisy = samp.sampling_sq_noisy(list_mesh, n_points=1000, noise_std=0.05, clip_k=3.0, seed=42)#list 
-    sampled_points_random = samp.sampling_sq_random(list_mesh, n_points=1000, seed=42) #list 
+    sampled_points_noisy = samp.sampling_sq_noisy(list_mesh, n_points=2000, noise_std=0.05, clip_k=3.0, seed=42)#list 
+    sampled_points_random = samp.sampling_sq_random(list_mesh, n_points=2000, seed=42) #list 
     sampled_points_outliers = samp.sampling_outliers(list_mesh, n_out=400, margin=0.10, mode="uniform", seed=42) #array 2D (N_out, 3)
 
     #------choose here which kind of points to use for fitting
-    sampled_points = np.vstack([*sampled_points_noisy])
+    sampled_points = np.vstack([*sampled_points_noisy]).astype(np.float32, copy=False)
     #theta0:InnerRansacResult = inner_ransac(sampled_points, refined_set_index=np.arange(sampled_points.shape[0]), actual_set_index=np.arange(sampled_points.shape[0]), threshold=0.1)
     #mesh_estimated = supmesh.superquadric_mesh(theta0.best_model)
 
-    small_sample = sampled_points_noisy[0][:20] # just for testing, should be sampled from the gair set
-    theta0:InnerRansacResult = fit_superquadric_ls(small_sample)
+    small_sample = sampled_points[:30] # just for testing, should be sampled from the gair set
+    theta0 = fit_superquadric_ls(small_sample)
     mesh_estimated = supmesh.superquadric_mesh(theta0)
     list_mesh.append(mesh_estimated)
     colors.append("lightgreen")
+
+    # use chamfer distance(one side) on ground-truth= sampled points(noisy) and the points from our supq estimated
+    sample_from_supq_estimated = np.asarray(samp.sampling_sq_random([mesh_estimated], n_points=2000, seed=50)[0],dtype=np.float32,)
+    _, index_sample_to_estimate = k_nearest_neighbors(sampled_points, sample_from_supq_estimated, k=1)
+    # One-sided distance: for each ground-truth point, measure the distance to its nearest point on the estimated shape.
+    cd_one_side = np.linalg.norm(sample_from_supq_estimated[index_sample_to_estimate] - sampled_points,axis=-1,ord=2,).mean()
+    print(f"one-side chamfer distance = {cd_one_side:.4f}")
+    # symmetric chamfer distance
+    cd=chamfer_distance(sampled_points,sample_from_supq_estimated)
+    print(f"symmetric chamfer distance = {cd:.4f}")
+
     vis.show_mesh_and_points(list_mesh, pts=sampled_points, point_size=5, show_bounds=True, colors=colors)
+
+def main(argv: list[str] | None = None) -> int:
+    if argv is None:
+        argv = sys.argv[1:]
     
+    create_and_estimate_supq()
 
     # GAIR-RANSAC: /TODO
     superquadric_list: list[SuperQuadricParams] = []
-  #  superquadric_list=
 
     return 0
 
